36653c033898096a9b5b8e9f7cabba147ed6be97
jack-ningtz <737276067@qq.com>
2021-12-11 13:39:18 +0800

    FactoryMethodDesignPattern

--- FactoryMethodDesignPattern/FactoryMethodDesignPattern.cs
+++ FactoryMethodDesignPattern/FactoryMethodDesignPattern.cs
@@ -2,11 +2,56 @@ using System;
 
 namespace  FactoryMethodDesignPattern
 {
-    class Pragrom
+    class Program
     {
-        static void Main(String[] args)
+        static void Main(string[] args)
         {
+            Product product1 = new ConcreteFactory1().CreateProduct();
+            product1.ShowInfo();
+            Product product2 = new ConcreteFactory2().CreateProduct();
+            product2.ShowInfo();        
+            Console.ReadKey();
+        }
+    }
+    public abstract class Product
+    {
+        public abstract void ShowInfo();
+    }
 
+    public class ConcreteProduct1 : Product
+    {
+        public override void ShowInfo()
+        {
+            Console.WriteLine("Product1");
+        }
+    }
+
+    public class ConcreteProduct2 : Product
+    {
+        public override void ShowInfo()
+        {
+            Console.WriteLine("Product2");
+        }
+    }
+
+    public abstract class AbstractFactory
+    {
+        public abstract Product CreateProduct();
+    }
+
+    public class ConcreteFactory1 : AbstractFactory
+    {
+        public override Product CreateProduct()
+        {
+            return new ConcreteProduct1();
+        }
+    }
+
+    public class ConcreteFactory2 : AbstractFactory
+    {
+        public override Product CreateProduct()
+        {
+            return new ConcreteProduct2();
         }
     }
 }
